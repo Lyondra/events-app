e26153982d1e1ed8b856e6e5557a9704f87820f1
DeerFriend <yaseminshn@gmail.com>
2021-08-20 17:34:01 +0100

    Adding API connection to TicketMaster to get real events

--- event.py
+++ event.py
@@ -1,16 +1,13 @@
-
-
-
 class Event:
-    def __init__(self, event_id, title: str, type: str, budget: str, city: str, address: str, image_path: str,
-                 datetime, likes: int, description: str):
+    def __init__(self, event_id: str, title: str, type: str, start_date_time: str, image_path: str, city: str,
+                 address: str, latitude, longitude, likes: int):
         self.event_id = event_id
         self.title = title
         self.type = type
-        self.budget = budget
+        self.start_date_time = start_date_time
+        self.image_path = image_path
         self.city = city
         self.address = address
-        self.image_path = image_path
-        self.datetime = datetime
+        self.latitude = latitude
+        self.longitude = longitude
         self.likes = likes
-        self.description = description
---
+++ event_converter.py
@@ -0,0 +1,42 @@
+from event import Event
+
+
+# event converter function that turns dictionary coming from ticketmaster api into an instance of Event class
+
+
+class EventConverter:
+    def __init__(self, default_likes):
+        # any settings useful for the converter can be put here, such as date format to be read if relevant
+        self.default_likes = default_likes
+
+    # convert an event such as the one from json/event_search_response_example.json into our class
+    def ticketmaster_event_to_app_event(self, ticketmaster_event_dict):
+        event_id = ticketmaster_event_dict["id"]
+        title = ticketmaster_event_dict["name"]
+
+        # Event type/classification
+        classification1 = ticketmaster_event_dict["classifications"][0]
+        event_type = classification1["segment"]["name"]
+
+        # images
+        image1 = ticketmaster_event_dict["images"][0]
+        image_path = image1["url"]
+
+        # date field
+        # might be better to convert to datetime instead, so that we can use better UI formats
+        event_start = ticketmaster_event_dict["dates"]["start"]
+        date = "" + event_start["localDate"] + " " + (event_start["localTime"] if "localTime" in event_start else "")
+
+        # Address/coordinates fields
+        venue1 = ticketmaster_event_dict["_embedded"]["venues"][0]
+        city = venue1["city"]["name"]
+        address = venue1["address"]["line1"] + ", " + venue1["postalCode"]
+        latitude = venue1["location"]["latitude"]
+        longitude = venue1["location"]["longitude"]
+
+        likes = self.default_likes  # will be overriden by DB
+
+        event = Event(event_id, title, event_type, date, image_path, city, address, latitude, longitude, likes)
+        return event
+
+    # TODO unit test for this method using the sample json file content
